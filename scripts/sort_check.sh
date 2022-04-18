0c79563df3c755722b84a8fd1fa4e2e152406bdd
donghanyuan <dhy_120@163.com>
2022-04-18 10:51:27 +0800

    add checker for problem 4

---
+++ scripts/sort_check.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# usage: ./sort_check.sh input.txt output.txt
+
+# string in list shouldn't contains any whitespaces.
+
+function sortstr() {
+    # `sort` has letter-case bug in manjaro, but works well in ubuntu.
+    echo "$1" | sed 's/\r//g' | tr ' ' '\n' | sort | tr '\n' ' ' | sed -e 's/[ ]*$//g'
+}
+
+TABLE=`cat $2 | sed 's/\r//g' | sed -n '1p'`
+
+echo '-'$TABLE'-'
+echo '-'`sortstr "$TABLE"`'-'
+
+SORTED_TABLE_STD=`sortstr "$TABLE"`
+SORTED_TABLE=`cat $2 | sed 's/\r//g' | sed -n '2p'`
+
+echo '-'$SORTED_TABLE_STD'-'
+echo '-'$SORTED_TABLE'-'
+
+if [ "$SORTED_TABLE_STD" != "$SORTED_TABLE" ]; then
+    echo "sort error"
+    exit 1
+fi
+
+INPUT_STR=`cat $1 | sed 's/\r//g' | sed -n '1p'`
+INSERT_TABLE=`echo -e "$TABLE $INPUT_STR"`
+SORTED_INSERT_TABLE_STD=`sortstr "$INSERT_TABLE"`
+SORTED_INSERT_TABLE=`cat $2 | sed 's/\r//g' | sed -n '4p'`
+
+if [ "$SORTED_INSERT_TABLE_STD" != "$SORTED_INSERT_TABLE" ]; then
+    echo "insert and sort error"
+    exit 1
+fi
+
+echo 'your answer is correct'
+
